eedeebae739e0c21f42ca4e7cefbf91e7857f59d
Trystan Newbury <trystan.newbury@gmail.com>
2023-08-10 13:30:16 +0100

    Test that /feedback endpoint should not accept empty feedback body: red state

--- tests/service_test.py
+++ tests/service_test.py
@@ -10,3 +10,8 @@ def client():
 def test_feedback_endpoint_returns_200(client):
     response = client.post("/feedback", json={"feedback": "The employee did well"})
     assert response.status_code == 200
+
+
+def test_feedback_endpoint_returns_400_given_no_feedback(client):
+    response = client.post("/feedback", json={})
+    assert response.status_code == 400
