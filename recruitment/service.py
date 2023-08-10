efbd76a10eac4e615b285c1d4f4f5e0eade0850a
Trystan Newbury <trystan.newbury@gmail.com>
2023-08-10 13:44:14 +0100

    Green test: /feedback endpoint returns 400 if no feedback is given

--- recruitment/service.py
+++ recruitment/service.py
@@ -1,8 +1,10 @@
-from flask import Flask, Response
+from flask import Flask, Response, request
 
 app = Flask(__name__)
 
 
 @app.post('/feedback')
 def feedback():
+    if request.json == {}:
+        return Response(status=400)
     return Response(status=200)
